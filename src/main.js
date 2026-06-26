66a480ddc0275956e1809f70649e988a87b5eea0
Bouzidi-Youssef <youssefbouzidi727@gmail.com>
2026-06-26 07:57:37 +0100

    remove recording code

--- src/main.js
+++ src/main.js
@@ -36,9 +36,6 @@ class DemoManagerScene extends Scene {
     super();
     this.demoIndex = 0;
     this.demo = null;
-    this._recording = false;
-    this._recorder = null;
-    this._chunks = [];
   }
 
   enter() {
@@ -64,47 +61,11 @@ class DemoManagerScene extends Scene {
     );
   }
 
-  _toggleRecording() {
-    if (this._recording) {
-      this._recorder.stop();
-    } else {
-      this._chunks = [];
-      const stream = this.game.canvas.captureStream(60);
-      const mime = MediaRecorder.isTypeSupported("video/mp4;codecs=h264")
-        ? "video/mp4;codecs=h264"
-        : MediaRecorder.isTypeSupported("video/webm;codecs=vp9")
-          ? "video/webm;codecs=vp9"
-          : MediaRecorder.isTypeSupported("video/webm;codecs=vp8")
-            ? "video/webm;codecs=vp8"
-            : "video/webm";
-      this._recorder = new MediaRecorder(stream, {
-        mimeType: mime,
-        videoBitsPerSecond: 100_000_000,
-      });
-      this._recorder.ondataavailable = (e) => {
-        if (e.data.size > 0) this._chunks.push(e.data);
-      };
-      this._recorder.onstop = () => {
-        const blob = new Blob(this._chunks, { type: "video/webm" });
-        const a = document.createElement("a");
-        a.href = URL.createObjectURL(blob);
-        const ext = mime.startsWith("video/mp4") ? "mp4" : "webm";
-        a.download = `${this.demo?.label ?? "particles"}-demo.${ext}`;
-        a.click();
-        this._recorder = null;
-      };
-      this._recorder.start();
-    }
-    this._recording = !this._recording;
-  }
-
   update(dt) {
     if (Input.justPressed("RIGHT")) {
       this.startDemo((this.demoIndex + 1) % demos.length);
     } else if (Input.justPressed("LEFT")) {
       this.startDemo((this.demoIndex - 1 + demos.length) % demos.length);
-    } else if (Input.justPressed("r")) {
-      this._toggleRecording();
     }
     this.demo?.update(dt);
   }
@@ -112,26 +73,14 @@ class DemoManagerScene extends Scene {
   render(ctx) {
     this.demo?.render(ctx);
 
+    ctx.fillStyle = "#ffffff60";
     ctx.font = "14px monospace";
     ctx.textAlign = "center";
-
-    if (this._recording) {
-      ctx.fillStyle = "#ff000080";
-      ctx.beginPath();
-      ctx.arc(20, 20, 6, 0, Math.PI * 2);
-      ctx.fill();
-    }
-
-    ctx.fillStyle = "#ffffff60";
     ctx.fillText(
       `◄  ${this.demo?.label ?? ""}  ►`,
       this.game.width / 2,
       this.game.height - 16,
     );
-
-    ctx.textAlign = "right";
-    ctx.fillStyle = this._recording ? "#ff666660" : "#ffffff40";
-    ctx.fillText(this._recording ? "● REC" : "[R] record", this.game.width - 12, 24);
   }
 }
 
